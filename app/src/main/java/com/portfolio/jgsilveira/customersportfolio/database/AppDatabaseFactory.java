1370719723ea5520fc13db82e5757f2c0fe00ad0
jonathangsilveira <jonathangsilveira@gmail.com>
2018-07-29 17:05:13 -0300

    Ajustes relatório

--- app/src/main/java/com/portfolio/jgsilveira/customersportfolio/database/AppDatabaseFactory.java
+++ app/src/main/java/com/portfolio/jgsilveira/customersportfolio/database/AppDatabaseFactory.java
@@ -3,12 +3,12 @@ package com.portfolio.jgsilveira.customersportfolio.database;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
 
-import com.portfolio.jgsilveira.customersportfolio.dao.ClienteDao;
-import com.portfolio.jgsilveira.customersportfolio.model.Cliente;
+import com.portfolio.jgsilveira.customersportfolio.dao.CustomerDao;
+import com.portfolio.jgsilveira.customersportfolio.model.Customer;
 
-@Database(entities = {Cliente.class}, version = 1)
+@Database(entities = {Customer.class}, version = 1, exportSchema = false)
 public abstract class AppDatabaseFactory extends RoomDatabase {
 
-    public abstract ClienteDao clienteDao();
+    public abstract CustomerDao clienteDao();
 
 }
